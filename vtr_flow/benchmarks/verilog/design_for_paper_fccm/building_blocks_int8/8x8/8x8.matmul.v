5b590c2ce5e24edc6ddf38404532737e387b220a
Aman Arora <aman.kbm@gmail.com>
2019-12-24 01:33:18 -0600

    Adding files/tasks for experiments with only the matrix multiplier (no memory). THis is needed for Table1/Fig1 in the results

---
+++ vtr_flow/benchmarks/verilog/design_for_paper_fccm/building_blocks_int8/8x8/8x8.matmul.v
@@ -0,0 +1,185 @@
+`timescale 1ns/1ns
+`define DWIDTH 8
+`define AWIDTH 7
+`define MEM_SIZE 128
+`define MAT_MUL_SIZE 4
+`define LOG2_MAT_MUL_SIZE 2
+`define BB_MAT_MUL_SIZE `MAT_MUL_SIZE
+`define NUM_CYCLES_IN_MAC 3
+
+
+/////////////////////////////////////////////////
+// The 8x8 matmul definition
+/////////////////////////////////////////////////
+
+module matmul_8x8_systolic(
+  clk,
+  done_mat_mul,
+  reset,
+  start_mat_mul,
+  a_data_0_0,
+  a_addr_0_0,
+  b_data_0_0,
+  b_addr_0_0,
+  a_data_1_0,
+  a_addr_1_0,
+  b_data_0_1,
+  b_addr_0_1,
+
+  c_data_row_0,
+  c_data_row_1
+);
+  input clk;
+  output done_mat_mul;
+
+  input reset;
+  input start_mat_mul;
+  input [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_0_0;
+  input [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_1_0;
+
+  input [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_0_0;
+  input [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_0_1;
+
+  output [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] c_data_row_0;
+  output [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] c_data_row_1;
+
+  output [`AWIDTH-1:0] a_addr_0_0;
+  output [`AWIDTH-1:0] a_addr_1_0;
+
+  output [`AWIDTH-1:0] b_addr_0_0;
+  output [`AWIDTH-1:0] b_addr_0_1;
+
+  /////////////////////////////////////////////////
+  // ORing all done signals
+  /////////////////////////////////////////////////
+  wire done_mat_mul_0_0;
+  wire done_mat_mul_0_1;
+  wire done_mat_mul_1_0;
+  wire done_mat_mul_1_1;
+
+  assign done_mat_mul = done_mat_mul_0_0;
+
+  /////////////////////////////////////////////////
+  // Matmul 0_0
+  /////////////////////////////////////////////////
+
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_0_0_to_0_1;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_0_0_to_1_0;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_in_0_0_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_in_0_0_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] c_data_0_0_to_0_1;
+
+matmul_4x4_systolic u_matmul_4x4_systolic_0_0(
+  .clk(clk),
+  .reset(reset),
+  .start_mat_mul(start_mat_mul),
+  .done_mat_mul(done_mat_mul_0_0),
+  .a_data(a_data_0_0),
+  .b_data(b_data_0_0),
+  .a_data_in(a_data_in_0_0_NC),
+  .b_data_in(b_data_in_0_0_NC),
+  .c_data_in({`BB_MAT_MUL_SIZE*`DWIDTH{1'b0}}),
+  .c_data_out(c_data_0_0_to_0_1),
+  .a_data_out(a_data_0_0_to_0_1),
+  .b_data_out(b_data_0_0_to_1_0),
+  .a_addr(a_addr_0_0),
+  .b_addr(b_addr_0_0),
+  .final_mat_mul_size(8'd8),
+  .a_loc(8'd0),
+  .b_loc(8'd0)
+);
+
+  /////////////////////////////////////////////////
+  // Matmul 0_1
+  /////////////////////////////////////////////////
+
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_0_1_to_0_2;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_0_1_to_1_1;
+  wire [`AWIDTH-1:0] a_addr_0_1_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_0_1_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_in_0_1_NC;
+
+matmul_4x4_systolic u_matmul_4x4_systolic_0_1(
+  .clk(clk),
+  .reset(reset),
+  .start_mat_mul(start_mat_mul),
+  .done_mat_mul(done_mat_mul_0_1),
+  .a_data(a_data_0_1_NC),
+  .b_data(b_data_0_1),
+  .a_data_in(a_data_0_0_to_0_1),
+  .b_data_in(b_data_in_0_1_NC),
+  .c_data_in(c_data_0_0_to_0_1),
+  .c_data_out(c_data_row_0),
+  .a_data_out(a_data_0_1_to_0_2),
+  .b_data_out(b_data_0_1_to_1_1),
+  .a_addr(a_addr_0_1_NC),
+  .b_addr(b_addr_0_1),
+  .final_mat_mul_size(8'd8),
+  .a_loc(8'd0),
+  .b_loc(8'd1)
+);
+
+  /////////////////////////////////////////////////
+  // Matmul 1_0
+  /////////////////////////////////////////////////
+
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_1_0_to_1_1;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_1_0_to_2_0;
+  wire [`AWIDTH-1:0] b_addr_1_0_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_1_0_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_in_1_0_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] c_data_1_0_to_1_1;
+
+matmul_4x4_systolic u_matmul_4x4_systolic_1_0(
+  .clk(clk),
+  .reset(reset),
+  .start_mat_mul(start_mat_mul),
+  .done_mat_mul(done_mat_mul_1_0),
+  .a_data(a_data_1_0),
+  .b_data(b_data_1_0_NC),
+  .a_data_in(a_data_in_1_0_NC),
+  .b_data_in(b_data_0_0_to_1_0),
+  .c_data_in({`BB_MAT_MUL_SIZE*`DWIDTH{1'b0}}),
+  .c_data_out(c_data_1_0_to_1_1),
+  .a_data_out(a_data_1_0_to_1_1),
+  .b_data_out(b_data_1_0_to_2_0),
+  .a_addr(a_addr_1_0),
+  .b_addr(b_addr_1_0_NC),
+  .final_mat_mul_size(8'd8),
+  .a_loc(8'd1),
+  .b_loc(8'd0)
+);
+
+  /////////////////////////////////////////////////
+  // Matmul 1_1
+  /////////////////////////////////////////////////
+
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_1_1_to_1_2;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_1_1_to_2_1;
+  wire [`AWIDTH-1:0] a_addr_1_1_NC;
+  wire [`AWIDTH-1:0] b_addr_1_1_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] a_data_1_1_NC;
+  wire [`BB_MAT_MUL_SIZE*`DWIDTH-1:0] b_data_1_1_NC;
+
+matmul_4x4_systolic u_matmul_4x4_systolic_1_1(
+  .clk(clk),
+  .reset(reset),
+  .start_mat_mul(start_mat_mul),
+  .done_mat_mul(done_mat_mul_1_1),
+  .a_data(a_data_1_1_NC),
+  .b_data(b_data_1_1_NC),
+  .a_data_in(a_data_1_0_to_1_1),
+  .b_data_in(b_data_0_1_to_1_1),
+  .c_data_in(c_data_1_0_to_1_1),
+  .c_data_out(c_data_row_1),
+  .a_data_out(a_data_1_1_to_1_2),
+  .b_data_out(b_data_1_1_to_2_1),
+  .a_addr(a_addr_1_1_NC),
+  .b_addr(b_addr_1_1_NC),
+  .final_mat_mul_size(8'd8),
+  .a_loc(8'd1),
+  .b_loc(8'd1)
+);
+
+endmodule
+
